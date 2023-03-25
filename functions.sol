944a9068ea212b2a80d143368cb144aa95664424
avg77 <91598955+avg77@users.noreply.github.com>
2023-03-25 19:34:55 +0530

    Create functions.sol

---
+++ functions.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract SimpleStorage
+{
+    //Functions
+
+    int256 public Aviral;
+
+    function store(int _Aviral) public       //four types of access specifiers:
+    {                                        //Public, Private, External and Internal
+        Aviral = _Aviral;                    //Internal is the default access specifier   
+        Aviral = Aviral + 1;                 //More the no. of statements, functions, more will be the gas fee  
+    }
+
+    // 'view' and 'pure' keywords in functions:-
+    // -> function to call, show or retrieve the value that we give to 'int _Aviral' in the previous function
+
+    function retrieve() public view returns(int)    //when we use view or pure keyword, gas is not applicable because we're just viewing a transaction already created on a blockchain and not making any changes to it
+    {                                               //however, if we call the function that use view or pure keyword in a gas calling function (function on which gas is applicable), then gas is charged on that function
+        return Aviral;
+    }
+
+    function add() public pure returns(int)
+    {
+        return (1+1);
+    }
+}
