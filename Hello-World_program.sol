6974e233dc39850cba4ce20869004b368c0407cd
avg77 <91598955+avg77@users.noreply.github.com>
2023-03-28 01:14:10 +0530

    Create Hello-World_program.sol

---
+++ Hello-World_program.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld 
+{   
+    function print() public pure returns (string memory) 
+    {       
+        return 'Hello World! This is my first simple Smart Contract';             
+    } 
+}
